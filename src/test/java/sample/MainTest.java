3778c5146fdf24d9353de37c99e284b7a6c08f57
Philipp Krüger <matheus.dev@gmail.com>
2016-11-08 18:00:08 +0100

    Add test stub

---
+++ src/test/java/sample/MainTest.java
@@ -0,0 +1,15 @@
+package sample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Philipp on 08.11.2016.
+ */
+public class MainTest {
+    @Test
+    public void testSomeLibraryMethod() {
+        // TODO: How to test???
+    }
+}
