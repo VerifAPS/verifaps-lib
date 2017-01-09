28206cf1ab107989b0daff816ebb508f0e946908
Philipp Krüger <matheus.dev@gmail.com>
2017-01-10 00:38:10 +0100

    Add code stubs for ConcreteSpecification

--- src/main/java/edu/kit/iti/formal/stvs/model/table/concrete/ConcreteCell.java
+++ src/main/java/edu/kit/iti/formal/stvs/model/table/concrete/ConcreteCell.java
@@ -1,7 +1,19 @@
 package edu.kit.iti.formal.stvs.model.table.concrete;
 
+import edu.kit.iti.formal.stvs.model.expressions.Value;
+
 /**
  * Created by philipp on 10.01.17.
  */
 public class ConcreteCell {
+
+    private final Value value;
+
+    public ConcreteCell(Value value) {
+        this.value = value;
+    }
+
+    public Value getValue() {
+        return value;
+    }
 }
