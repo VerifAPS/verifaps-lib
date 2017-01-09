7b80d8de919d24b0afe9b56d053847d7c0c17b64
Benjamin Alt <benjamin_alt@outlook.com>
2017-01-09 22:57:38 +0100

    Added verification stuff to model

---
+++ src/main/java/edu/kit/iti/formal/stvs/logic/verification/VerificationEngine.java
@@ -0,0 +1,33 @@
+package edu.kit.iti.formal.stvs.logic.verification;
+
+import edu.kit.iti.formal.stvs.logic.io.VerificationExporter;
+import edu.kit.iti.formal.stvs.logic.io.VerificationImporter;
+import edu.kit.iti.formal.stvs.model.code.Code;
+import edu.kit.iti.formal.stvs.model.verification.VerificationResult;
+import edu.kit.iti.formal.automation.testtables; //TODO this doesn't import despite Maven repo
+
+import java.util.function.Consumer;
+
+/**
+ * Handles communication with the ExTeTa verification engine
+ */
+public class VerificationEngine {
+    private VerificationExporter exporter;
+    private VerificationImporter importer;
+    private VerificationResult result;
+    private Consumer<VerificationResult> verificationFinishedListener;
+    private ExTeTa exteta;
+
+    public VerificationEngine(Consumer<VerificationResult> verificationFinishedListener) {
+
+    }
+
+    /**
+     * Starts a verification in its own thread
+     * @param code The code to verify
+     * @param spec The specification to verify against
+     */
+    public void startVerification(Code code, ConstraintSpecification spec) {
+
+    }
+}
---
+++ src/main/java/edu/kit/iti/formal/stvs/logic/verification/VerificationThread.java
@@ -0,0 +1,20 @@
+package edu.kit.iti.formal.stvs.logic.verification;
+
+import edu.kit.iti.formal.stvs.model.code.Code;
+
+/**
+ * Created by bal on 09.01.17.
+ */
+public class VerificationThread extends Thread {
+
+    private Code code;
+    private ConstraintSpecification specification;
+
+    public VerificationThread(Code code, ConstraintSpecification specification) {
+
+    }
+
+    public void run() {
+
+    }
+}
