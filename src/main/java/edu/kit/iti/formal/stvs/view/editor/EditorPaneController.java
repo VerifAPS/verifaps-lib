ff48ae7135873d29e73abc8fe1fbee82e0a8760b
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2017-01-10 02:16:24 +0100

    fixed final errors

--- src/main/java/edu/kit/iti/formal/stvs/view/editor/EditorPaneController.java
+++ src/main/java/edu/kit/iti/formal/stvs/view/editor/EditorPaneController.java
@@ -1,13 +1,15 @@
 package edu.kit.iti.formal.stvs.view.editor;
 
 import edu.kit.iti.formal.stvs.model.code.Code;
+import org.fxmisc.richtext.StyleSpans;
 
 /**
  * Created by csicar on 09.01.17.
  */
 public class EditorPaneController implements OnCodeModelChangeListener {
-    public EditorPaneController(CodeModel codeModel) {
-        codeModel.setOnCodeChangeListener(this);
+    public EditorPaneController(Code code) {
+
+        // code.addChangeListener(this);
     };
     public void onCodeChange(Code code) {
 
--- src/main/java/edu/kit/iti/formal/stvs/view/spec/SpecificationsPaneController.java
+++ src/main/java/edu/kit/iti/formal/stvs/view/spec/SpecificationsPaneController.java
@@ -1,6 +1,7 @@
 package edu.kit.iti.formal.stvs.view.spec;
 
 import edu.kit.iti.formal.stvs.model.common.IOVariable;
+import edu.kit.iti.formal.stvs.model.expressions.Type;
 import edu.kit.iti.formal.stvs.model.table.hybrid.HybridSpecification;
 import javafx.collections.ObservableList;
 
@@ -11,6 +12,7 @@ public class SpecificationsPaneController {
     private ObservableList<HybridSpecification> hybridSpecifications;
 
     public ObservableList<HybridSpecification> getHybridSpecifications() {
+        return null;
     }
 
     private ObservableList<Type> types;
