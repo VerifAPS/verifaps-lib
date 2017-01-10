6d8a515b744fa18c92cd3b9590acf055c5488604
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2017-01-10 15:58:55 +0100

    file choosers added

---
+++ src/main/java/edu/kit/iti/formal/stvs/view/menu/STFileChooserController.java
@@ -0,0 +1,12 @@
+package edu.kit.iti.formal.stvs.view.menu;
+
+import edu.kit.iti.formal.stvs.model.code.Code;
+
+/**
+ * Created by csicar on 10.01.17.
+ */
+public class STFileChooserController {
+    public STFileChooserController(Code code) {
+
+    }
+}
