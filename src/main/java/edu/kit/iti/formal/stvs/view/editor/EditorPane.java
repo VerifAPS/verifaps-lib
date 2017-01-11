7a2e18a3a9447703bbd69449204f8d03d39dbd6d
Leon Kaucher <leon.kaucher@student.kit.edu>
2017-01-11 22:09:48 +0100

    Added folding to editor

--- src/main/java/edu/kit/iti/formal/stvs/view/editor/EditorPane.java
+++ src/main/java/edu/kit/iti/formal/stvs/view/editor/EditorPane.java
@@ -1,8 +1,9 @@
 package edu.kit.iti.formal.stvs.view.editor;
 
-import edu.kit.iti.formal.stvs.model.code.SourcecodeToken;
 import javafx.beans.property.StringProperty;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import org.fxmisc.richtext.CodeArea;
 import org.fxmisc.richtext.StyleSpans;
 
@@ -12,12 +13,16 @@ import java.util.List;
 /**
  * Created by csicar on 09.01.17.
  *
- * Some parts are inspired by examples of the used library:
- * https://github.com/TomasMikula/RichTextFX/blob/a098da6309a0f624052fd1d4d6f5079dd6265fbe/richtextfx-demos/src/main/java/org/fxmisc/richtext/demo/JavaKeywords.java
  */
 public class EditorPane extends Pane {
-    StringProperty code;
+    private StringProperty code;
+
+    /**
+     * Contains ButtonList and CodeArea
+     */
+    private HBox horizontalBox;
     private CodeArea codeArea;
+    private VBox buttonList;
 
 
     public StringProperty getCodeProperty() {
@@ -32,7 +37,17 @@ public class EditorPane extends Pane {
 
     }
 
+
+
     public void setStyleSpans(StyleSpans<Collection<String>> style){
 
+    }
+
+    public CodeArea getCodeArea() {
+        return codeArea;
+    }
+
+    public VBox getButtonList() {
+        return buttonList;
     }
 }
