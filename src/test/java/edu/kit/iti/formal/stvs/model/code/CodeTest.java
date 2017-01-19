42a813e9df9b0882fb155e728a3d6106a7983ca8
Philipp Krüger <matheus.dev@gmail.com>
2017-01-19 14:59:53 +0100

    Reformat test tabs to 2 spaces, add test st file

--- src/test/java/edu/kit/iti/formal/stvs/model/code/CodeTest.java
+++ src/test/java/edu/kit/iti/formal/stvs/model/code/CodeTest.java
@@ -16,37 +16,37 @@ import static org.junit.Assert.assertTrue;
  */
 public class CodeTest {
 
-    private final Code code = new Code();
-    private final Code exampleCode = new Code("stfile.st", "THIS IS SPARTA");
+  private final Code code = new Code();
+  private final Code exampleCode = new Code("stfile.st", "THIS IS SPARTA");
 
-    @Test
-    public void testIsEmptyInitially() {
-        assertEquals("", code.sourcecodeProperty().get());
-    }
+  @Test
+  public void testIsEmptyInitially() {
+    assertEquals("", code.sourcecodeProperty().get());
+  }
 
-    @Test
-    public void testSourcecodeListenable() {
-        BooleanProperty sourcecodeChanged = new SimpleBooleanProperty(false);
-        code.sourcecodeProperty().addListener(change -> sourcecodeChanged.set(true));
-        code.sourcecodeProperty().set("TYPE months := (jan, feb) END_TYPE");
-        assertEquals("Sourcecode changed", true, sourcecodeChanged.get());
-    }
+  @Test
+  public void testSourcecodeListenable() {
+    BooleanProperty sourcecodeChanged = new SimpleBooleanProperty(false);
+    code.sourcecodeProperty().addListener(change -> sourcecodeChanged.set(true));
+    code.sourcecodeProperty().set("TYPE months := (jan, feb) END_TYPE");
+    assertEquals("Sourcecode changed", true, sourcecodeChanged.get());
+  }
 
-    @Test
-    public void testTokensExist() {
-        code.sourcecodeProperty().set("TYPE is a keyword END_TYPE");
-        List<? extends Token> tokens = code.tokensBinding().getValue();
-        System.out.println(tokens);
-        assertTrue(tokens.size() > 0);
-    }
+  @Test
+  public void testTokensExist() {
+    code.sourcecodeProperty().set("TYPE is a keyword END_TYPE");
+    List<? extends Token> tokens = code.tokensBinding().getValue();
+    System.out.println(tokens);
+    assertTrue(tokens.size() > 0);
+  }
 
-    @Test
-    public void testTokensConcatenated() {
-        String source = exampleCode.sourcecodeProperty().get();
-        List<? extends Token> tokens = exampleCode.tokensBinding().getValue();
-        String tokensConcatenated = tokens.stream()
-                .map(Token::getText)
-                .reduce("", String::concat);
-        assertEquals("Lexer tokens concatenated are source code", source, tokensConcatenated);
-    }
+  @Test
+  public void testTokensConcatenated() {
+    String source = exampleCode.sourcecodeProperty().get();
+    List<? extends Token> tokens = exampleCode.tokensBinding().getValue();
+    String tokensConcatenated = tokens.stream()
+        .map(Token::getText)
+        .reduce("", String::concat);
+    assertEquals("Lexer tokens concatenated are source code", source, tokensConcatenated);
+  }
 }
