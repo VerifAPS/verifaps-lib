6df9e9b2f5beffb775e1ebb235dffc5d065682b1
Philipp Krüger <matheus.dev@gmail.com>
2017-01-29 16:16:11 +0100

    Make STVS compile again!

--- src/main/java/edu/kit/iti/formal/stvs/view/spec/variables/VariableView.java
+++ src/main/java/edu/kit/iti/formal/stvs/view/spec/variables/VariableView.java
@@ -1,9 +1,14 @@
 package edu.kit.iti.formal.stvs.view.spec.variables;
 
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 
 /**
  * Created by csicar on 10.01.17.
  */
 public class VariableView extends HBox {
+
+  public VariableView() {
+    this.getChildren().add(new Label("THIS IS MY VARIABLE"));
+  }
 }
