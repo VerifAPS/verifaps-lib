3a83c2a114615c54feb15a605076fd2dd9a3ce60
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2017-02-07 17:01:09 +0100

    added listtypeconverter

---
+++ src/main/java/edu/kit/iti/formal/stvs/util/ListTypeConverter.java
@@ -0,0 +1,28 @@
+package edu.kit.iti.formal.stvs.util;
+
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.ObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * Created by csicar on 07.02.17.
+ */
+public class ListTypeConverter {
+
+  public static <E> ObservableList<E> makeObservableList(ObjectProperty<List<E>> list) {
+    ObservableList<E> observableList = FXCollections.observableList(list.get());
+    list.addListener((observableValue, newList, oldList) -> {
+      observableList.clear();
+      observableList.addAll(newList);
+    });
+    return observableList;
+  }
+
+}
---
+++ src/main/java/edu/kit/iti/formal/stvs/view/spec/SpecificationsPane.java
@@ -0,0 +1,7 @@
+package edu.kit.iti.formal.stvs.view.spec;
+
+/**
+ * Created by csicar on 07.02.17.
+ */
+public class SpecificationsPane {
+}
