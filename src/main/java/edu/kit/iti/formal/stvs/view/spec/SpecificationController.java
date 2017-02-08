3d72765b2fe1497cc1434e9ea5eaa94495e9ee07
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2017-02-08 08:08:12 +0100

    added timingdiagram to view

--- src/main/java/edu/kit/iti/formal/stvs/view/spec/SpecificationController.java
+++ src/main/java/edu/kit/iti/formal/stvs/view/spec/SpecificationController.java
@@ -1,5 +1,8 @@
 package edu.kit.iti.formal.stvs.view.spec;
 
+import edu.kit.iti.formal.stvs.logic.io.xml.XmlConcreteSpecImporter;
+import edu.kit.iti.formal.stvs.model.common.Selection;
+import edu.kit.iti.formal.stvs.model.table.ConcreteSpecification;
 import edu.kit.iti.formal.stvs.model.table.HybridSpecification;
 import edu.kit.iti.formal.stvs.model.verification.VerificationState;
 import edu.kit.iti.formal.stvs.view.Controller;
@@ -8,6 +11,9 @@ import edu.kit.iti.formal.stvs.view.spec.timingdiagram.TimingDiagramCollectionCo
 import edu.kit.iti.formal.stvs.view.spec.variables.VariableCollectionController;
 import javafx.beans.property.ObjectProperty;
 import javafx.scene.control.ContextMenu;
+
+import java.io.File;
+import java.io.FileInputStream;
 
 public class SpecificationController implements Controller {
 
@@ -18,13 +24,17 @@ public class SpecificationController implements Controller {
   private VariableCollectionController variableCollectionController;
   private SpecificationTableController tableController;
   private TimingDiagramCollectionController timingDiagramCollectionController;
+  private Selection selection;
+  private  HybridSpecification hybridSpecification;
 
   public SpecificationController(
       HybridSpecification hybridSpecification,
       ObjectProperty<VerificationState> stateProperty) {
     this.spec = hybridSpecification;
+    this.hybridSpecification = hybridSpecification;
     this.stateProperty = stateProperty;
     this.view = new SpecificationView();
+    this.selection = new Selection();
     this.variableCollectionController = new VariableCollectionController(
         hybridSpecification.typeContextProperty(),
         hybridSpecification.getFreeVariableSet());
@@ -32,9 +42,19 @@ public class SpecificationController implements Controller {
         hybridSpecification.typeContextProperty(),
         hybridSpecification.codeIoVariablesProperty(),
         hybridSpecification.getFreeVariableSet()); //TODO: Use actual hybridSpecification and take its content
-    //TODO this.timingDiagramCollectionController = new TimingDiagramCollectionController()
+
+    if(getConcreteSpecification() != null) {
+      this.timingDiagramCollectionController = new TimingDiagramCollectionController
+          (getConcreteSpecification(), selection);
+      view.setDiagram(timingDiagramCollectionController.getView());
+    }
     view.setVariableCollection(variableCollectionController.getView());
     view.setTable(tableController.getView());
+  }
+
+  private ConcreteSpecification getConcreteSpecification() {
+    return hybridSpecification.getConcreteInstance() == null ? hybridSpecification
+        .getConcreteInstance() : hybridSpecification.getConcreteInstance();
   }
 
   public SpecificationView getView() {
