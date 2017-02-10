106d75f2d53e075978b17bb93b4495d14e3a632e
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2017-02-10 18:43:27 +0100

    test updated for new api

--- src/test/java/edu/kit/iti/formal/stvs/logic/specification/smtlib/SmtPreprocessorTest.java
+++ src/test/java/edu/kit/iti/formal/stvs/logic/specification/smtlib/SmtPreprocessorTest.java
@@ -3,7 +3,7 @@ package edu.kit.iti.formal.stvs.logic.specification.smtlib;
 import edu.kit.iti.formal.stvs.logic.io.ImportException;
 import edu.kit.iti.formal.stvs.logic.io.ImporterFacade;
 import edu.kit.iti.formal.stvs.model.common.CodeIoVariable;
-import edu.kit.iti.formal.stvs.model.common.FreeVariable;
+import edu.kit.iti.formal.stvs.model.common.FreeVariableListValidator;
 import edu.kit.iti.formal.stvs.model.common.ValidFreeVariable;
 import edu.kit.iti.formal.stvs.model.expressions.Type;
 import edu.kit.iti.formal.stvs.model.expressions.TypeBool;
@@ -12,20 +12,19 @@ import edu.kit.iti.formal.stvs.model.expressions.TypeInt;
 import edu.kit.iti.formal.stvs.model.table.ConstraintSpecification;
 import edu.kit.iti.formal.stvs.model.table.ValidSpecification;
 import edu.kit.iti.formal.stvs.model.table.problems.SpecProblemRecognizer;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.property.SimpleObjectProperty;
 import org.junit.Test;
 
 import java.util.*;
-import java.util.stream.Collectors;
-
-import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by csicar on 09.02.17.
  */
 public class SmtPreprocessorTest {
+  private List<ValidFreeVariable> freeVariables;
 
-  private ValidSpecification importSpec(String name, List<ValidFreeVariable> freeVariables) throws
+  private ValidSpecification importSpec(String name) throws
       ImportException {
     List<Type> typeContext = Arrays.asList(TypeInt.INT, TypeBool.BOOL, new TypeEnum("colors",
         Arrays.asList("red", "green", "blue")));
@@ -33,17 +32,23 @@ public class SmtPreprocessorTest {
 
     ConstraintSpecification constraintSpec = ImporterFacade.importSpec(getClass().getResourceAsStream(name), ImporterFacade
         .ImportFormat.XML);
+    FreeVariableListValidator freeVariableListValidator = new FreeVariableListValidator(new
+        SimpleObjectProperty<>(typeContext), constraintSpec
+        .getFreeVariableList());
+    List<ValidFreeVariable> freeVariables = freeVariableListValidator.validFreeVariablesProperty().get();
+    this.freeVariables = freeVariables;
     SpecProblemRecognizer recognizer = new SpecProblemRecognizer(
-        new SimpleObjectProperty<>(typeContext), new SimpleObjectProperty<>(codeIoVariables),
-        new SimpleObjectProperty<>(freeVariables), constraintSpec);
+        new SimpleObjectProperty<>(typeContext),
+        new SimpleObjectProperty<>(codeIoVariables),
+        new ReadOnlyObjectWrapper<>(freeVariables),
+        constraintSpec);
     return recognizer.getValidSpecification();
   }
 
   @Test
   public void testImported() throws ImportException {
-    List<ValidFreeVariable> freeVariables = new LinkedList<>();
 
-    ValidSpecification spec = importSpec("testSpec.xml", freeVariables);
+    ValidSpecification spec = importSpec("testSpec.xml");
 
     Map<Integer, Integer> maxDurations = new HashMap<Integer,
         Integer>() {{
