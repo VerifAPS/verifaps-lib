10378ae6d2f51f0c4f50ff6d0b04b057b60ed30c
Leon Kaucher <leon.kaucher@student.kit.edu>
2017-02-12 17:06:50 +0100

    Z3Solver Test independent from Javafx

--- src/test/java/edu/kit/iti/formal/stvs/logic/specification/smtlib/Z3SolverTest.java
+++ src/test/java/edu/kit/iti/formal/stvs/logic/specification/smtlib/Z3SolverTest.java
@@ -4,12 +4,8 @@ import edu.kit.iti.formal.stvs.model.common.ValidIoVariable;
 import edu.kit.iti.formal.stvs.model.common.VariableCategory;
 import edu.kit.iti.formal.stvs.model.expressions.TypeBool;
 import edu.kit.iti.formal.stvs.model.expressions.TypeInt;
+import edu.kit.iti.formal.stvs.model.expressions.ValueInt;
 import edu.kit.iti.formal.stvs.model.table.ConcreteSpecification;
-import edu.kit.iti.formal.stvs.util.AsyncTask;
-import edu.kit.iti.formal.stvs.view.JavaFxTest;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -17,21 +13,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by leonk on 09.02.2017.
  */
 public class Z3SolverTest {
-
-  private static String TESTSTRING = "; This example illustrates basic arithmetic and \n" +
-      "; uninterpreted functions\n" +
-      "\n" +
-      "(declare-fun A_0_0 () Int)\n" +
-      "(declare-fun B_0_0 () Int)\n" +
-      "(declare-fun A_1_0 () Int)\n" +
-
-      "(check-sat)\n" +
-      "(get-model)\n" +
-      "(exit)";
 
   private static String TEST2 = "(declare-const A_0_0 Int)\n" +
       "(declare-const B_0_0 Bool)\n" +
@@ -42,29 +31,16 @@ public class Z3SolverTest {
       "(get-value (A_0_0 B_0_0 n_0))";
 
   @Test
-  public void testTask() {
-    JavaFxTest.setToBeViewed(this::simpleScene);
-    Application.launch(JavaFxTest.class);
-  }
-
-  private Scene simpleScene() {
-    TextArea root = new TextArea();
+  public void testSimpleConcretize() throws IOException {
     List<ValidIoVariable> validIoVariables = new ArrayList<>();
     validIoVariables.add(new ValidIoVariable(VariableCategory.INPUT, "A", TypeInt.INT));
     validIoVariables.add(new ValidIoVariable(VariableCategory.OUTPUT, "B", TypeBool.BOOL));
-    AsyncTask<Optional<ConcreteSpecification>> asysncTask = new AsyncTask<>(
-        () -> {
-          Optional<ConcreteSpecification> concrete = Optional.empty();
-          try {
-            concrete = Z3Solver.concretizeVarAssignment(TEST2, validIoVariables);
-          } catch (IOException e) {
-            e.printStackTrace();
-          }
-          return concrete;
-        },
-        (optionalConcreteSpec) -> System.out.println(optionalConcreteSpec)
-    );
-    asysncTask.run();
-    return new Scene(root, 800, 600);
+    Optional<ConcreteSpecification> concreteSpecificationOptional = Z3Solver.concretizeVarAssignment(TEST2, validIoVariables);
+    System.out.println(concreteSpecificationOptional);
+    assertTrue(concreteSpecificationOptional.isPresent());
+    ConcreteSpecification concreteSpecification = concreteSpecificationOptional.get();
+    assertEquals(1,concreteSpecification.getDurations().get(0).getDuration());
+    assertEquals(10, ((ValueInt)concreteSpecification.getColumnByName("A").getCells().get(0).getValue()).getValue());
+    assertNotNull(concreteSpecification.getColumnByName("B").getCells().get(0));
   }
 }
