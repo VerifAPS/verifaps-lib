7c9e971d4b08b0af093cfcf9cb2719ce5129c17a
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2017-02-13 13:24:33 +0100

    created interface between SPecificationConcretizer and the z3 solver

--- src/main/java/edu/kit/iti/formal/stvs/logic/specification/smtlib/SMTConcretizer.java
+++ src/main/java/edu/kit/iti/formal/stvs/logic/specification/smtlib/SMTConcretizer.java
@@ -1,48 +1,59 @@
 package edu.kit.iti.formal.stvs.logic.specification.smtlib;
 
-import edu.kit.iti.formal.stvs.logic.specification.ConcretizerContext;
 import edu.kit.iti.formal.stvs.logic.specification.SpecificationConcretizer;
-import edu.kit.iti.formal.stvs.model.common.NullableProperty;
+import edu.kit.iti.formal.stvs.logic.specification.SpecificationConcretizerState;
+import edu.kit.iti.formal.stvs.model.common.ValidFreeVariable;
+import edu.kit.iti.formal.stvs.model.config.GlobalConfig;
 import edu.kit.iti.formal.stvs.model.table.ConcreteSpecification;
 import edu.kit.iti.formal.stvs.model.table.ValidSpecification;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
 
 /**
  * Created by csicar on 08.02.17.
  */
 public class SMTConcretizer implements SpecificationConcretizer {
   private ValidSpecification validSpecification;
+  private GlobalConfig config;
+  private Optional<ConcreteSpecification> concreteSpecification;
+  private SmtEncoder encoder;
+  private List<Consumer<SpecificationConcretizerState>> eventListeners;
+  private SpecificationConcretizerState concretizerState;
 
-  public SMTConcretizer() {
-
-  }
-
-  @Override
-  public ConcretizerContext getContext() {
-    return null;
-  }
-
-  @Override
-  public void setContext(ConcretizerContext context) {
-
-  }
-
-  @Override
-  public void createConcreteSpecification(ValidSpecification spec) {
+  public SMTConcretizer(ValidSpecification validSpecification, GlobalConfig config,
+                        List<ValidFreeVariable> freeVariables) {
+    this.validSpecification = validSpecification;
+    this.config = config;
+    this.encoder = new SmtEncoder((i) -> config.getMaxLineRollout(), validSpecification,
+        freeVariables);
+    this.concreteSpecification = Optional.empty();
+    this.concretizerState = SpecificationConcretizerState.IDLE;
 
   }
 
   @Override
-  public ConcreteSpecification getConcreteSpec() {
-    return null;
+  public Optional<ConcreteSpecification> calculateConcreteSpecification() throws IOException {
+    this.concreteSpecification = Z3Solver.concretizeSConstraint(encoder
+        .getConstrain(), validSpecification.getColumnHeaders());
+    this.concretizerState = SpecificationConcretizerState.FINISHED;
+    fireListeners();
+    return this.concreteSpecification;
   }
 
   @Override
-  public NullableProperty<ConcreteSpecification> concreteSpecProperty() {
-    return null;
+  public Optional<ConcreteSpecification> getConcreteSpecification() {
+    return concreteSpecification;
+  }
+
+  private void fireListeners() {
+    eventListeners.forEach(eventListener -> eventListener.accept(this.concretizerState));
   }
 
   @Override
-  public void setConcreteSpec(ConcreteSpecification concreteSpec) {
-
+  public void addEventListener(Consumer<SpecificationConcretizerState> eventListener) {
+    eventListeners.add(eventListener);
   }
 }
