ffc9b5740aee8df7c25e0607d00362a944c1ae54
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2017-02-16 21:36:00 +0100

    made rows commentable

--- src/main/java/edu/kit/iti/formal/stvs/view/spec/table/CommentPopup.java
+++ src/main/java/edu/kit/iti/formal/stvs/view/spec/table/CommentPopup.java
@@ -1,14 +1,59 @@
 package edu.kit.iti.formal.stvs.view.spec.table;
 
+import edu.kit.iti.formal.stvs.view.spec.Lockable;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.StringProperty;
+import javafx.geometry.Insets;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
+import javafx.scene.web.HTMLEditor;
 import javafx.stage.Popup;
 
-public class CommentPopup extends Popup {
-  private static Text commentHeadline;
-  private TextField commentField;
+public class CommentPopup extends Dialog<String> implements Lockable {
+  private TextArea commentField;
+  private String commentContent;
+  private final BooleanProperty editable;
 
-  public TextField getCommentField() {
+  public CommentPopup(String commentContent) {
+    super();
+    this.setTitle("Edit Comment");
+    this.setContentText("Comment");
+    this.commentContent = commentContent;
+    this.commentField = new TextArea(commentContent);
+    editable = new SimpleBooleanProperty();
+    GridPane grid = new GridPane();
+    grid.setHgap(10);
+    grid.setVgap(10);
+    grid.setPadding(new Insets(20, 150, 10, 10));
+    grid.add(commentField, 0, 0);
+
+    this.getDialogPane().getButtonTypes().addAll(ButtonType.APPLY, ButtonType.CANCEL);
+    this.getDialogPane().setContent(grid);
+
+
+  }
+
+  public TextArea getCommentField() {
     return commentField;
+  }
+
+  @Override
+  public boolean getEditable() {
+    return editable.get();
+  }
+
+  @Override
+  public void setEditable(boolean b) {
+    editable.set(b);
+  }
+
+  @Override
+  public BooleanProperty getEditableProperty() {
+    return editable;
   }
 }
