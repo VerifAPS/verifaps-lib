5169b3495319916917ad8d8cc0a735baafdae9f8
Leon Kaucher <leon.kaucher@student.kit.edu>
2017-02-16 14:53:16 +0100

    removed unused files in timingdiagram

--- src/main/java/edu/kit/iti/formal/stvs/view/spec/timingdiagram/CategoryTimingDiagramCollectionView.java
+++
@@ -1,9 +0,0 @@
-package edu.kit.iti.formal.stvs.view.spec.timingdiagram;
-
-import javafx.scene.layout.VBox;
-
-/**
- * Created by csicar on 09.01.17.
- */
-public class CategoryTimingDiagramCollectionView extends VBox {
-}
