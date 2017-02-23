33e1590d732b359431548a2e14015806f1290dd7
Leon Kaucher <leon.kaucher@student.kit.edu>
2017-02-23 17:47:03 +0100

    clean util lambdas

--- src/main/java/edu/kit/iti/formal/stvs/util/ProcessOutputAsyncTask.java
+++ src/main/java/edu/kit/iti/formal/stvs/util/ProcessOutputAsyncTask.java
@@ -20,27 +20,29 @@ public class ProcessOutputAsyncTask extends AsyncTask<Optional<String>> {
 
   private static Function<AtomicBoolean, Optional<String>> createProcessHandler(ProcessBuilder processBuilder, String input) {
     //The new line chars are all transformed into a single \n
-    return (isRunning) -> {
-      String result = "";
-      try {
-        Process process = processBuilder.start();
-        PrintStream printStream = new PrintStream(process.getOutputStream());
-        printStream.print(input);
-        printStream.close();
-        final BufferedReader reader = new BufferedReader(
-            new InputStreamReader(process.getInputStream()));
-        String line;
-        while ((line = reader.readLine()) != null && isRunning.get()) {
-          result += line + "\n";
-        }
-        if (isRunning.get()) {
-          return Optional.of(result);
-        } else {
-          return Optional.empty();
-        }
-      } catch (IOException e) {
-        throw new RuntimeException(e);
+    return (isRunning) -> runProcessWhileThreadRunning(processBuilder, input, isRunning);
+  }
+
+  private static Optional<String> runProcessWhileThreadRunning(ProcessBuilder processBuilder, String input, AtomicBoolean isRunning) {
+    String result = "";
+    try {
+      Process process = processBuilder.start();
+      PrintStream printStream = new PrintStream(process.getOutputStream());
+      printStream.print(input);
+      printStream.close();
+      final BufferedReader reader = new BufferedReader(
+          new InputStreamReader(process.getInputStream()));
+      String line;
+      while ((line = reader.readLine()) != null && isRunning.get()) {
+        result += line + "\n";
       }
-    };
+      if (isRunning.get()) {
+        return Optional.of(result);
+      } else {
+        return Optional.empty();
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
