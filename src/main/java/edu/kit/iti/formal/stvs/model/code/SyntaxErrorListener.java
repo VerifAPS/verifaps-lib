d4724d6535039bf129987d873f38418c44779358
Lukas Fritsch <l.fritsch97@googlemail.com>
2017-03-08 10:09:32 +0100

    fixed done TODO in SyntaxErrorListener

--- src/main/java/edu/kit/iti/formal/stvs/model/code/SyntaxErrorListener.java
+++ src/main/java/edu/kit/iti/formal/stvs/model/code/SyntaxErrorListener.java
@@ -22,10 +22,6 @@ public class SyntaxErrorListener implements ANTLRErrorListener {
   @Override
   public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line,
       int charPositionInLine, String msg, RecognitionException exc) {
-    // TODO: RecognitionException is null when there are "expected ';'" errors, for example
-    // (these errors dont have a token (which makes sense), so we have to come up with a nice
-    // way to visualize them in the EditorPane)
-    // syntaxErrors.add(new SyntaxError(exc.getOffendingToken()));
     syntaxErrors.add(new SyntaxError(line, charPositionInLine, msg));
   }
 
