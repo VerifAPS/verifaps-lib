a063bb516a63a402d3a2676a01bcd317be0cde36
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2017-03-21 11:16:31 +0100

    added test for previously fixed bug

--- src/test/java/edu/kit/iti/formal/stvs/logic/io/VariableEscaperTest.java
+++ src/test/java/edu/kit/iti/formal/stvs/logic/io/VariableEscaperTest.java
@@ -31,6 +31,46 @@ public class VariableEscaperTest {
   }
 
   @Test
+  public void testEscapeEnumReference() {
+    Code code = new Code();
+    code.updateSourcecode("TYPE\n" +
+        "  TrafficLight : (Red, Green);\n" +
+        "END_TYPE\n" +
+        "\n" +
+        "PROGRAM PedestrianTrafficLight\n" +
+        "\n" +
+        "  VAR_INPUT\n" +
+        "    pedBtnPress : BOOL;\n" +
+        "  END_VAR\n" +
+        "  \n" +
+        "  VAR_OUTPUT\n" +
+        "    carLight : TrafficLight;\n" +
+        "    pedLight : TrafficLight;\n" +
+        "    counter  : INT;\n" +
+        "  END_VAR\n" +
+        "  carLight := TrafficLight#Red;\n" +
+        "END_PROGRAM");
+    String expected = "TYPE\n" +
+        "  var_TrafficLight : (var_Red, var_Green);\n" +
+        "END_TYPE\n" +
+        "\n" +
+        "PROGRAM var_PedestrianTrafficLight\n" +
+        "\n" +
+        "  VAR_INPUT\n" +
+        "    var_pedBtnPress : BOOL;\n" +
+        "  END_VAR\n" +
+        "  \n" +
+        "  VAR_OUTPUT\n" +
+        "    var_carLight : var_TrafficLight;\n" +
+        "    var_pedLight : var_TrafficLight;\n" +
+        "    var_counter  : INT;\n" +
+        "  END_VAR\n" +
+        "  var_carLight := var_TrafficLight#var_Red;\n" +
+        "END_PROGRAM";
+    assertEquals(expected, VariableEscaper.escapeCode(code));
+  }
+
+  @Test
   public void testEscapeCode() {
     Code code = new Code();
     code.updateSourcecode("TYPE\n" +
