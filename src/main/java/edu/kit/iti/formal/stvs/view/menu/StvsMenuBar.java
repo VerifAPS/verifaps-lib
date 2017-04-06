746976bc403cf2bc72d6ffd9bdc2d3353938ba72
Alexander Weigl <weigl@kit.edu>
2017-04-07 01:38:11 +0200

    simplified menubar

--- src/main/java/edu/kit/iti/formal/stvs/view/menu/StvsMenuBar.java
+++ src/main/java/edu/kit/iti/formal/stvs/view/menu/StvsMenuBar.java
@@ -9,7 +9,6 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.input.KeyCombination;
 
-
 /**
  * Created by csicar on 10.01.17.
  *
@@ -17,78 +16,79 @@ import javafx.scene.input.KeyCombination;
  */
 public class StvsMenuBar extends MenuBar {
 
-  public Menu file;
-  public Menu edit;
-  public Menu help;
-  public Menu examples;
+    public Menu file;
+    //public Menu edit;
+    public Menu help;
+    public Menu examples;
 
-  public Menu openOther;
-  public Menu openRecent;
-  public List<MenuItem> openRecentItems;
+    public Menu openOther;
+    public Menu openRecent;
+    public List<MenuItem> openRecentItems;
 
-  public MenuItem open;
-  public MenuItem openSpec;
-  public MenuItem openCode;
-  public MenuItem openSession;
-  public MenuItem saveCode;
-  public MenuItem saveSpec;
-  public MenuItem saveAll;
-  public MenuItem config;
-  public MenuItem newCode;
-  public MenuItem newSpec;
-  public MenuItem saveSessionAs;
-  public MenuItem about;
+    public MenuItem open;
+    public MenuItem openSpec;
+    public MenuItem openCode;
+    public MenuItem openSession;
+    public MenuItem saveCode;
+    public MenuItem saveSpec;
+    public MenuItem saveAll;
+    public MenuItem config;
+    public MenuItem newCode;
+    public MenuItem newSpec;
+    public MenuItem saveSessionAs;
+    public MenuItem about;
 
-  /**
-   * Menu bar at the top of the window.
-   */
-  public StvsMenuBar() {
-    // create top-level menus
-    file = new Menu("File");
-    examples = new Menu("Examples");
-    edit = new Menu("Edit");
-    help = new Menu("Help");
+    /**
+     * Menu bar at the top of the window.
+     */
+    public StvsMenuBar() {
+        // create top-level menus
+        file = new Menu("_File");
+        examples = new Menu("_Examples");
+        //edit = new Menu("Edit");
+        help = new Menu("Help");
 
-    // create menu-items
-    open = new MenuItem("Open");
-    open.setAccelerator(KeyCombination.keyCombination("Ctrl+o"));
-    openOther = new Menu("Open ...");
-    openSpec = new MenuItem("Open Specification");
-    openCode = new MenuItem("Open Code");
-    openSession = new MenuItem("Open Session");
-    openRecent = new Menu("Open Recent...");
-    openRecentItems = new ArrayList<>();
+        // create menu-items
+        open = new MenuItem("_Open");
+        open.setAccelerator(KeyCombination.keyCombination("Ctrl+o"));
+        openOther = new Menu("Open ...");
+        openSpec = new MenuItem("Open Specification");
+        openCode = new MenuItem("Open Code");
+        openSession = new MenuItem("Open Session");
+        openRecent = new Menu("Open Recent...");
+        openRecentItems = new ArrayList<>();
 
-    saveCode = new MenuItem("Save Code");
-    saveSpec = new MenuItem("Save Specification");
+        saveCode = new MenuItem("_Save Code");
+        saveSpec = new MenuItem("Save Specification");
 
-    saveSessionAs = new MenuItem("Save Session As");
-    saveSessionAs.setAccelerator(KeyCombination.keyCombination("Shift+Ctrl+s"));
+        saveSessionAs = new MenuItem("Save Session As");
+        saveSessionAs
+                .setAccelerator(KeyCombination.keyCombination("Shift+Ctrl+s"));
 
-    saveAll = new MenuItem("Save Session");
-    saveAll.setAccelerator(KeyCombination.keyCombination("Ctrl+s"));
+        saveAll = new MenuItem("Save Session");
+        saveAll.setAccelerator(KeyCombination.keyCombination("Ctrl+s"));
 
-    config = new MenuItem("Preferences");
-    config.setAccelerator(KeyCombination.keyCombination("Ctrl+,"));
+        config = new MenuItem("_Preferences");
+        config.setAccelerator(KeyCombination.keyCombination("Ctrl+,"));
 
+        newCode = new MenuItem("New Code");
+        newSpec = new MenuItem("New Specification");
 
-    newCode = new MenuItem("New Code");
-    newSpec = new MenuItem("New Specification");
+        about = new MenuItem("About");
 
-    about = new MenuItem("About");
+        // Add menu items to "open other" menu
+        openOther.getItems().addAll(openSpec, openCode, openSession);
 
-    // Add menu items to "open other" menu
-    openOther.getItems().addAll(openSpec, openCode, openSession);
+        // Add menu items to "file" menu
+        file.getItems().addAll(newCode, newSpec, open, openOther, openRecent,
+                (new SeparatorMenuItem()), saveCode, saveSpec, saveAll,
+                saveSessionAs, (new SeparatorMenuItem()), config);
 
-    // Add menu items to "file" menu
-    file.getItems().addAll(newCode, newSpec, open, openOther, openRecent, (new SeparatorMenuItem()),
-        saveCode, saveSpec, saveAll, saveSessionAs);
+        //edit.getItems().addAll(config);
 
-    edit.getItems().addAll(config);
+        help.getItems().addAll(about);
 
-    help.getItems().addAll(about);
-
-    // Add menus to menubar
-    this.getMenus().addAll(file, examples, edit, help);
-  }
+        // Add menus to menubar
+        this.getMenus().addAll(file, examples, help);
+    }
 }
