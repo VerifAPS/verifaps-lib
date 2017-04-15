1354551833932791480e30f76d0f820d68b7b6cd
Alexander Weigl <weigl@kit.edu>
2017-04-15 13:45:19 +0200

    Preparing 1.2.0
    * tag: v1.2.0-beta
    * version is included in the jar file name
    * change "courier" to monospace
    * clean up and simplifications

--- src/main/java/edu/kit/iti/formal/stvs/view/menu/AboutDialogPane.java
+++ src/main/java/edu/kit/iti/formal/stvs/view/menu/AboutDialogPane.java
@@ -1,6 +1,7 @@
 package edu.kit.iti.formal.stvs.view.menu;
 
 import edu.kit.iti.formal.stvs.StvsApplication;
+import edu.kit.iti.formal.stvs.StvsVersion;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
@@ -12,12 +13,12 @@ import javafx.scene.text.Font;
  * <p>
  * The Dialog Pane that shows 'About' information.
  * </p>
- *
+ * <p>
  * <p>
  * Is created when the user clicks on 'Help' -> 'About' and shows the name and version information,
  * etc.
  * </p>
- *
+ * <p>
  * <p>
  * Created by csicar on 16.02.17.
  * </p>
@@ -26,22 +27,25 @@ import javafx.scene.text.Font;
  */
 public class AboutDialogPane extends DialogPane {
 
-  private VBox content;
+    private VBox content;
 
-  /**
-   * Creates the Dialog Pane that displays 'About' information.
-   */
-  public AboutDialogPane() {
-    Image logo = new Image(StvsApplication.class.getResourceAsStream("logo.png"));
-    ImageView logoView = new ImageView(logo);
-    Label name = new Label("Structured Text Verification Studio - STVS");
-    name.setFont(Font.font("DejaVu Sans Mono", 30));
-    Label version = new Label("Version: 1.1");
+    /**
+     * Creates the Dialog Pane that displays 'About' information.
+     */
+    public AboutDialogPane() {
+        Image logo = new Image(
+                StvsApplication.class.getResourceAsStream("logo.png"));
+        ImageView logoView = new ImageView(logo);
+        Label name = new Label("Structured Text Verification Studio - STVS");
+        name.setFont(Font.font("DejaVu Sans Mono", 30));
+        Label version = new Label(
+                "Version: " + StvsVersion.getVersion() + " built from "
+                        + StvsVersion.getBuildId());
 
-    this.content = new VBox(logoView, name, version);
-    this.content.setAlignment(Pos.CENTER);
-    this.getButtonTypes().addAll(ButtonType.CLOSE);
-    this.setContent(content);
+        this.content = new VBox(logoView, name, version);
+        this.content.setAlignment(Pos.CENTER);
+        this.getButtonTypes().addAll(ButtonType.CLOSE);
+        this.setContent(content);
 
-  }
+    }
 }
