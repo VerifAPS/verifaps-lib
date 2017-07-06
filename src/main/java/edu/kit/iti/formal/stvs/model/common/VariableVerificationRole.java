4128ca4dd015b146f73ef9da43908a696ae8df11
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2017-07-06 11:49:27 +0200

    updated VariableCategory and created VariableVerificationRole

---
+++ src/main/java/edu/kit/iti/formal/stvs/model/common/VariableVerificationRole.java
@@ -0,0 +1,11 @@
+package edu.kit.iti.formal.stvs.model.common;
+
+/**
+ * Created by csicar on 06.07.17.
+ * specifies what role (can be assume or assert) a variable should be used as.
+ * ASSUME can be used for VariableCategories INPUT, INOUT, LOCAL
+ * ASSERT can be used for VariableCategories OUTPUT, INOUT, LOCAl
+ */
+public enum VariableVerificationRole {
+  ASSUME, ASSERT
+}
