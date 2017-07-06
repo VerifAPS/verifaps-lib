51d1576ebdf7c8b38d0c23a5a35890d101aee87d
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2017-07-06 12:38:36 +0200

    renamed VariableVerificationRole to VariableRole to make it less verbose

--- src/main/java/edu/kit/iti/formal/stvs/model/common/VariableRole.java
+++ src/main/java/edu/kit/iti/formal/stvs/model/common/VariableRole.java
@@ -2,10 +2,11 @@ package edu.kit.iti.formal.stvs.model.common;
 
 /**
  * Created by csicar on 06.07.17.
- * specifies what role (can be assume or assert) a variable should be used as.
+ * specifies what role (can be assume or assert) a variable should be used as in Verification and
+ * Concretization
  * ASSUME can be used for VariableCategories INPUT, INOUT, LOCAL
  * ASSERT can be used for VariableCategories OUTPUT, INOUT, LOCAl
  */
-public enum VariableVerificationRole {
+public enum VariableRole {
   ASSUME, ASSERT
 }
