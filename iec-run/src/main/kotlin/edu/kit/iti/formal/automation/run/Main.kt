43f4313f8af9230fdda80c492adea35a909d6fba
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2017-09-12 17:50:12 +0200

    removed unused files (Main.kt, ValuesVisitor.kt)

--- iec-run/src/main/kotlin/edu/kit/iti/formal/automation/run/Main.kt
+++
@@ -1,9 +0,0 @@
-package edu.kit.iti.formal.automation.run
-
-import edu.kit.iti.formal.automation.parser.IEC61131Parser
-
-infix fun String.test(v: String) = v == this
-
-fun main(args: Array<String>) {
-    println(1L + 1.0F is Float)
-}
--- iec-run/src/main/kotlin/edu/kit/iti/formal/automation/run/ValueVisitor.kt
+++
@@ -1,5 +0,0 @@
-package edu.kit.iti.formal.automation.run
-
-interface ValueVisitor<T> {
-   fun visit()
-}
