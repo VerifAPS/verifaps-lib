47f4e7122243e9383aee24a522a981a31d2f0839
Augusto Modanese <augusto.modanese@student.kit.edu>
2017-11-05 12:58:19 +0100

    Revise find instances analysis
    Add an Instance class to wrap variable declaration and the top level
    element it was declared in.
    
    Find instances in a hierarchical manner instead of just listing them.
    For example, if A is a class with an attribute B and there are two
    instances of A, then there should be two instances of B, each with one
    of the two A instances as its parent.
    
    Finally, have the find instances analysis start from a single top level
    element. In printTopLevelElements, assume this is the (first) program
    declaration; this is suitable for our test code.

--- iec61131lang/src/main/java/edu/kit/iti/formal/automation/scope/InstanceScope.java
+++ iec61131lang/src/main/java/edu/kit/iti/formal/automation/scope/InstanceScope.java
@@ -23,6 +23,7 @@
 package edu.kit.iti.formal.automation.scope;
 
 import edu.kit.iti.formal.automation.st.ast.*;
+import lombok.Data;
 import lombok.ToString;
 
 import java.io.Serializable;
@@ -39,91 +40,91 @@ import java.util.Map;
 @ToString
 public class InstanceScope implements Serializable {
     private final GlobalScope globalScope;
-    private Map<ClassDeclaration, List<VariableDeclaration>> classInstances = new HashMap<>();
-    private Map<FunctionBlockDeclaration, List<VariableDeclaration>> functionBlockInstances = new HashMap<>();
-    private Map<InterfaceDeclaration, List<VariableDeclaration>> interfaceInstances = new HashMap<>();
-    private Map<ClassDeclaration, List<VariableDeclaration>> classPolymorphInstances = new HashMap<>();
-    private Map<FunctionBlockDeclaration, List<VariableDeclaration>> functionBlockPolymorphInstances = new HashMap<>();
+    private Map<ClassDeclaration, List<Instance>> classInstances = new HashMap();
+    private Map<FunctionBlockDeclaration, List<Instance>> functionBlockInstances = new HashMap();
+    private Map<InterfaceDeclaration, List<Instance>> interfaceInstances = new HashMap();
+    private Map<ClassDeclaration, List<Instance>> classPolymorphInstances = new HashMap();
+    private Map<FunctionBlockDeclaration, List<Instance>> functionBlockPolymorphInstances = new HashMap();
 
     public InstanceScope(GlobalScope globalScope) {
         this.globalScope = globalScope;
         for (InterfaceDeclaration interfaceDeclaration : globalScope.getInterfaces())
-            interfaceInstances.put(interfaceDeclaration, new ArrayList<>());
+            interfaceInstances.put(interfaceDeclaration, new ArrayList());
         for (ClassDeclaration classDeclaration : globalScope.getClasses()) {
-            classInstances.put(classDeclaration, new ArrayList<>());
-            classPolymorphInstances.put(classDeclaration, new ArrayList<>());
+            classInstances.put(classDeclaration, new ArrayList());
+            classPolymorphInstances.put(classDeclaration, new ArrayList());
         }
         for (FunctionBlockDeclaration functionBlockDeclaration : globalScope.getFunctionBlocks()) {
-            functionBlockInstances.put(functionBlockDeclaration, new ArrayList<>());
-            functionBlockPolymorphInstances.put(functionBlockDeclaration, new ArrayList<>());
+            functionBlockInstances.put(functionBlockDeclaration, new ArrayList());
+            functionBlockPolymorphInstances.put(functionBlockDeclaration, new ArrayList());
         }
     }
 
     /**
      * @return The instances of a class, disregarding polymorphy.
      */
-    public List<VariableDeclaration> getInstancesOfClass(String className) {
+    public List<Instance> getInstancesOfClass(String className) {
         return getInstancesOfClass(globalScope.resolveClass(className));
     }
 
     /**
      * @return The instances of a class, disregarding polymorphy.
      */
-    public List<VariableDeclaration> getInstancesOfClass(ClassDeclaration classDeclaration) {
+    public List<Instance> getInstancesOfClass(ClassDeclaration classDeclaration) {
         return classInstances.get(classDeclaration);
     }
 
     /**
      * @return The instances of a function block, disregarding polymorphy.
      */
-    public List<VariableDeclaration> getInstancesOfFunctionBlock(String functionBlockName) {
+    public List<Instance> getInstancesOfFunctionBlock(String functionBlockName) {
         return getInstancesOfFunctionBlock(globalScope.getFunctionBlock(functionBlockName));
     }
 
     /**
      * @return The instances of a function block, disregarding polymorphy.
      */
-    public List<VariableDeclaration> getInstancesOfFunctionBlock(FunctionBlockDeclaration functionBlockDeclaration) {
+    public List<Instance> getInstancesOfFunctionBlock(FunctionBlockDeclaration functionBlockDeclaration) {
         return functionBlockInstances.get(functionBlockDeclaration);
     }
 
     /**
      * @return The instances which are compatible with the given interface.
      */
-    public List<VariableDeclaration> getInstancesOfInterface(String interfaceName) {
+    public List<Instance> getInstancesOfInterface(String interfaceName) {
         return getInstancesOfInterface(globalScope.resolveInterface(interfaceName));
     }
 
     /**
      * @return The instances which are compatible with the given interface.
      */
-    public List<VariableDeclaration> getInstancesOfInterface(InterfaceDeclaration interfaceDeclaration) {
+    public List<Instance> getInstancesOfInterface(InterfaceDeclaration interfaceDeclaration) {
         return interfaceInstances.get(interfaceDeclaration);
     }
 
     /**
      * @return The instances which can have the given class as their type. Takes polymorphy into account.
      */
-    public List<VariableDeclaration> getPolymorphInstancesOfClass(ClassDeclaration classDeclaration) {
+    public List<Instance> getPolymorphInstancesOfClass(ClassDeclaration classDeclaration) {
         return classPolymorphInstances.get(classDeclaration);
     }
 
     /**
      * @return The instances which can have the given function block as their type. Takes polymorphy into account.
      */
-    public List<VariableDeclaration> getPolymorphInstancesOfFunctionBlock(
+    public List<Instance> getPolymorphInstancesOfFunctionBlock(
             FunctionBlockDeclaration functionBlockDeclaration) {
         return functionBlockPolymorphInstances.get(functionBlockDeclaration);
     }
 
-    public void registerClassInstance(ClassDeclaration classDeclaration, VariableDeclaration instance) {
+    public void registerClassInstance(ClassDeclaration classDeclaration, Instance instance) {
         classInstances.get(classDeclaration).add(instance);
         classDeclaration.getImplementedInterfaces().forEach(i -> interfaceInstances.get(i).add(instance));
         classDeclaration.getExtendedClasses().forEach(c -> classPolymorphInstances.get(c).add(instance));
     }
 
     public void registerFunctionBlockInstance(FunctionBlockDeclaration functionBlockDeclaration,
-                                              VariableDeclaration instance) {
+                                              Instance instance) {
         functionBlockInstances.get(functionBlockDeclaration).add(instance);
         functionBlockDeclaration.getImplementedInterfaces().forEach(i -> interfaceInstances.get(i).add(instance));
         functionBlockDeclaration.getExtendedClasses().forEach(c -> {
@@ -133,5 +134,18 @@ public class InstanceScope implements Serializable {
                 // Function blocks may also extend classes
                 classPolymorphInstances.get(c).add(instance);
         });
+    }
+
+    /**
+     * Model an instance of a class or function block. Maintains a reference to where the instance is declared.
+     */
+    @Data
+    public static class Instance {
+        /**
+         * Null iff parent is a program.
+         */
+        private final Instance parent;
+
+        private final VariableDeclaration variableDeclaration;
     }
 }
