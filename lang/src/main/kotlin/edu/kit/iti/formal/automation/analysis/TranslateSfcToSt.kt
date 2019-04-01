39d5a8cb7b24e1ef84924158c9aedf6312a1efa5
Clon451 <15787600+Clon451@users.noreply.github.com>
2019-04-01 02:57:50 +0200

    SFC to ST translation written

--- lang/src/main/kotlin/edu/kit/iti/formal/automation/analysis/TranslateSfcToSt.kt
+++ lang/src/main/kotlin/edu/kit/iti/formal/automation/analysis/TranslateSfcToSt.kt
@@ -2,18 +2,19 @@ package edu.kit.iti.formal.automation.analysis
 
 import edu.kit.iti.formal.automation.IEC61131Facade
 import edu.kit.iti.formal.automation.st.ast.ActionDeclaration
-import edu.kit.iti.formal.automation.st.ast.FunctionDeclaration
+import edu.kit.iti.formal.automation.st.ast.FunctionBlockDeclaration
 import edu.kit.iti.formal.automation.st.ast.ProgramDeclaration
 import edu.kit.iti.formal.automation.st.util.AstVisitorWithScope
 
 object TranslateSfcToSt : AstVisitorWithScope<Unit>() {
     override fun defaultVisit(obj: Any) {}
+
     override fun visit(programDeclaration: ProgramDeclaration) {
         super.visit(programDeclaration)
         programDeclaration.sfcBody?.also {
             if (programDeclaration.stBody == null) {
                 programDeclaration.stBody = IEC61131Facade
-                        .translateToSt("", programDeclaration.scope, it)
+                        .translateToSt(programDeclaration.name, programDeclaration.scope, it)
             }
         }
     }
@@ -21,18 +22,17 @@ object TranslateSfcToSt : AstVisitorWithScope<Unit>() {
     override fun visit(actionDeclaration: ActionDeclaration) {
         actionDeclaration.sfcBody?.also {
             if (actionDeclaration.stBody == null) {
-                actionDeclaration.stBody = IEC61131Facade.translateToSt(actionDeclaration.name,
-                        this.scope, it)
+                actionDeclaration.stBody = IEC61131Facade.translateToSt(actionDeclaration.name, this.scope, it)
             }
         }
     }
 
-    override fun visit(functionDeclaration: FunctionDeclaration) {
-        super.visit(functionDeclaration)
-        functionDeclaration.sfcBody?.also {
-            if (functionDeclaration.stBody == null) {
-                functionDeclaration.stBody = IEC61131Facade.translateToSt("",
-                        functionDeclaration.scope, it)
+    override fun visit(functionBlockDeclaration: FunctionBlockDeclaration) {
+        super.visit(functionBlockDeclaration)
+        functionBlockDeclaration.sfcBody?.also {
+            if (functionBlockDeclaration.stBody == null) {
+                functionBlockDeclaration.stBody = IEC61131Facade.translateToSt(functionBlockDeclaration.name,
+                        functionBlockDeclaration.scope, it)
             }
         }
     }
